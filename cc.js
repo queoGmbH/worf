0d71c814cf2b933a9c8d2f48cbd09fd32fd32ad1
ujamii.boerner <lb@ujamii.com>
2020-03-16 15:32:26 +0100

    [FEATURE] implement better user input

--- cc.js
+++ cc.js
@@ -2,38 +2,38 @@ const puppeteer = require('puppeteer');
 const fs = require('fs').promises;
 const path = require('path');
 const yaml = require('js-yaml');
-
-const rl = require('readline').createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
-
-const filename = 'cookies';
+let prompt = require('prompt');
 
 let data = {
 	url: {
+		name: 'url',
 		value: '',
 		validator: '^(https?:\/\/)(www\.)?[a-zA-Z0-9#=@:%._\+-]{1,256}(\.[a-zA-Z0-9]{1,6}\/?)([a-zA-Z0-9#=@:%._\+-\/]*)$',
+		warning: 'Enter a Url',
 		required: true
 	},
 	inputFile: {
+		name: 'inputFile',
 		value: '',
 		validator: '^.{1,}\..{2,}$',
-		required: false,
 	},
 	outputPath: {
+		name: 'outputPath',
 		value: '',
-		validator: '^.*[\/]*.\/$', 	// [a-zA-Z0-9+/#.]*$
+		validator: '^.*[\/]*.\/$',
 		required: false,
 		default: './'
 	},
 	depth: {
+		name: 'depth',
 		value: '',
 		validator: '^[0-9]*$',
 		required: false,
-		default: '0'
+		default: 0,
+		warning: 'Please enter a number'
 	},
 	mode: {
+		name: 'mode',
 		value: '',
 		validator: '^(deep|fast)$',
 		required: false,
@@ -41,34 +41,37 @@ let data = {
 	}
 };
 
+const filename = "cookies";
+
 // PUPPETEER
 let browser;
-let page;
 
+let page;
 // TEMPORARY VARIABLES
 let currentDepth = 0;
+
 let startTime;
 let visited = [];
-let missingProperties = [];
+
+let prompt_attributes = [];
 
 // REGULAR EXPRESSIONS
 let regularExpressionInput = new RegExp('^-[a-z]$');
-const regExpDomain = new RegExp(data.url.value.split('/')[2]);
+let regExpDomain;
 const regExpMail = new RegExp('@');
 
-(async () => {
-
-	sortInput(process.argv);
-
-	//await startProcess();
-})();
+sortInput(process.argv);
 
 function sortInput(input) {
+	// Fill data with the given input
 	for (let i = 0; i < input.length; i++) {
 		for (let property in data) {
 			if (data.hasOwnProperty(property)) {
+				// Check if an value is like -u oder -d ...
 				if (regularExpressionInput.test(input[i]) && input[i].length === 2) {
+					// Check if the input is equal to properties to define
 					if (input[i] === '-' + property.slice(0, 1)) {
+						// If all is equal then get next value of input values and add it to data
 						data[property].value = input[i + 1];
 					}
 				}
@@ -77,78 +80,47 @@ function sortInput(input) {
 	}
 
 	for (let property in data) {
-		if(data.hasOwnProperty(property)) {
-			if (data[property].value.length < 1) {
-				missingProperties.push(property);
+		if (data.hasOwnProperty(property)) {
+			if (data[property].value.length === 0) {
+				prompt_attributes.push(data[property]);
 			}
 		}
 	}
 
-	if(missingProperties.length > 0) {
-		(function getMissingProperty() {
-			const missingProperty = missingProperties.shift();
-			if(data[missingProperty].required === true) {
-				rl.question(missingProperty + ': ', (answer) => {
-					if (validateData(missingProperty, answer)) {
-						data[missingProperty].value = answer;
-					} else {
-						console.log('\nInvalid argument\n');
-						missingProperties.push(missingProperty);
-					}
-					if (missingProperties.length > 0) {
-						getMissingProperty();
-					} else {
-						displayInput(data.url.value, data.inputFile.value, data.outputPath.value, data.depth.value, data.mode.value);
-						rl.close()
-					}
-				});
-			} else {
-				if(data[missingProperty.hasOwnProperty('default')])data[missingProperty].value = data[missingProperty].default;
-			}
-		})();
-	} else {
-		displayInput(data.url.value, data.inputFile.value, data.outputPath.value, data.depth.value, data.mode.value);
-	}
-}
+	prompt.start();
 
-function validateData(property, answer) {
-	if(answer.length > 0) return new RegExp(data[property].validator).test(answer);
-}
-
-
-function displayInput(url, inputFile, outputPath, depth, mode) {
-	rl.close();
-	console.log('\nurl:          ' + url);
-	console.log('inputFile:    ' + inputFile);
-	console.log('outputPath:   ' + outputPath);
-	console.log('depth:        ' + depth);
-	console.log('mode:         ' + mode + '\n');
-
-	startProcess();
-}
-
-async function startProcess() {
-	if (data.url.value != null) {
-		if (!isNaN(data.depth.value)) {
-			try {
-				startTime = new Date(Date.now()).getTime();
-
-				browser = await puppeteer.launch();
-				page = await browser.newPage();
-
-				await loopOverUrls([data.url.value]);
-			} catch (error) {
-				console.log(error);
-			} finally {
-				await browser.close();
-			}
+	prompt.get(prompt_attributes, function (err, result) {
+		if (err) {
+			console.log(err);
+			return 1;
 		} else {
-			console.log('No Depth is given');
+			for (let property in result) {
+				if (result.hasOwnProperty(property)) {
+					data[property].value = result[property];
+				}
+			}
+
+			regExpDomain = new RegExp('^(https?://)(www.)?' + data.url.value.split('/')[2] + '(.[a-zA-Z0-9]{1,6}/?)([a-zA-Z0-9#=@:%._+-/]*)$');
+			startProcess([data.url.value]);
 		}
-	} else {
-		console.log('No Url is given');
+	});
+}
+
+async function startProcess(url) {
+	try {
+		startTime = new Date(Date.now()).getTime();
+
+		browser = await puppeteer.launch();
+		page = await browser.newPage();
+
+		await loopOverUrls(url);
+	} catch (error) {
+		console.log(error);
+	} finally {
+		await browser.close();
 	}
 }
+
 
 async function loopOverUrls(givenUrls) {
 	let newUrls = [];
@@ -158,8 +130,7 @@ async function loopOverUrls(givenUrls) {
 			console.log('\nVisit ' + givenUrls.length + ' Urls at Depth ' + currentDepth);
 			for (let i = 0; i < givenUrls.length; i++) {
 				if (visited.indexOf(givenUrls[i]) === -1) {
-					console.log(givenUrls[i]);
-					if (data.mode.value === 'fast') {
+					if (data.mode.value === "fast") {
 						await page.goto(givenUrls[i]);
 					} else {
 						await page.goto(givenUrls[i], {waitUntil: 'networkidle2'});
@@ -181,18 +152,23 @@ async function loopOverUrls(givenUrls) {
 				}
 			}
 
-			console.log('Found ' + newUrls.length + ' new Urls');
+			if (newUrls.length > 0) {
+				console.log('Found ' + newUrls.length + ' new Urls');
+			} else {
+				console.log('No new Urls found');
+			}
 		}
 
 		currentDepth++;
 
-		if (newUrls.length) {
-			await loopOverUrls(newUrls);
-		}
+
+		await loopOverUrls(newUrls);
+
 	} else if (currentDepth > data.depth.value) {
 		await getCookies();
 	}
 }
+
 
 async function getUrls() {
 	try {
@@ -205,7 +181,7 @@ async function getUrls() {
 
 function filterUrls(input) {
 	input = input.filter(item => regExpDomain.test(item));
-	input = input.filter(item => item.substr(item.length - 4, 1) !== '.');
+	input = input.filter(item => item.substr(item.length - 4, 1) !== ".");
 	input = input.filter(item => !regExpMail.test(item));
 	input = input.map(i => {
 		return i.split('#')[0];
@@ -219,9 +195,11 @@ function filterUrls(input) {
 		}
 	});
 	input = input.filter(item => item != null);
+	input = input.filter(item => item.length > 0);
 
 	return input;
 }
+
 
 async function getCookies() {
 	const response = await page._client.send('Network.getAllCookies');
@@ -262,9 +240,10 @@ async function getCookies() {
 	await writeFile(cookies);
 }
 
+
 async function writeFile(cookies) {
 	if (cookies.length > 0) {
-		let foundedCookies = cookies.length;
+		let cookieLength = cookies.length;
 		cookies = formatCookies(cookies);
 		let yamlStr = yaml.safeDump(await yamlStringConstructor(cookies));
 		const filePath = path.join(data.outputPath.value, filename + '.yaml');
@@ -272,10 +251,8 @@ async function writeFile(cookies) {
 
 		console.log('\nVisited Urls:               ' + visited.length);
 		console.log('Total time required:        ' + timeStringConstructor());
-		console.log('Cookies found:              ' + foundedCookies);
-		console.log('Yaml file saved to:         ' + filePath + '\n');
-
-		rl.close();
+		console.log('Cookies found:              ' + cookieLength);
+		console.log('Yaml file saved to:         ' + filePath + "\n");
 	} else {
 		console.log('No Cookies found at the given url');
 	}
@@ -294,7 +271,7 @@ function formatCookies(cookies) {
 }
 
 async function yamlStringConstructor(cookies) {
-	if(data.inputFile.value != null) {
+	if(data.inputFile.value.length > 0) {
 		let string = await readFile();
 		string.cookie_registry.cookies = cookies;
 		return string;
@@ -307,6 +284,7 @@ async function readFile() {
 	let fileContents = await fs.readFile(data.inputFile.value, 'utf8');
 	return yaml.safeLoad(fileContents);
 }
+
 
 function timeStringConstructor() {
 	let seconds = (new Date(Date.now()).getTime() - startTime) / 1000;
